a654348a8049f0a882c5f82e26a7937c0e492a27
AutoCode <autocode@papergo.io>
2024-07-09 18:28:23 +0300

    update from autocode on 2024-07-09 18:28:23.656183

--- Types/ApiTag.cs
+++ Types/ApiTag.cs
@@ -1,14 +1,11 @@
 
+using System;
+
 namespace ProjectName.Types
 {
     public class ApiTag
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public int? Version { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime? Changed { get; set; }
-        public Guid CreatorId { get; set; }
-        public Guid? ChangedUser { get; set; }
     }
 }
