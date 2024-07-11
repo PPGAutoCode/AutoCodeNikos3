a6e42fdc6ba4a4e8baf3087c4d84db832dcb8411
AutoCode <autocode@papergo.io>
2024-07-11 16:30:52 +0300

    update from autocode on 2024-07-11 16:30:52.626377

--- Types/UpdateApiTagDto.cs
+++ Types/UpdateApiTagDto.cs
@@ -5,8 +5,6 @@ namespace ProjectName.Types
     {
         public Guid? Id { get; set; }
         public string? Name { get; set; }
-        public int? Version { get; set; }
-        public DateTime? Changed { get; set; }
         public Guid? ChangedUser { get; set; }
     }
 }
