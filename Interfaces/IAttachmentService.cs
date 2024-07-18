ebc2ce48561bb8800896aaa6cff06ce43af2c5d0
AutoCode <autocode@papergo.io>
2024-07-18 16:58:49 +0300

    update from autocode on 2024-07-18 16:58:49.288042

--- Interfaces/IAttachmentService.cs
+++ Interfaces/IAttachmentService.cs
@@ -1,36 +1,56 @@
-
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using ProjectName.Types;
 
 namespace ProjectName.Interfaces
 {
+    /// <summary>
     /// Interface for managing attachments.
+    /// </summary>
     public interface IAttachmentService
     {
-        /// Creates a new attachment
-        /// <param name="createAttachmentDto">The data transfer object containing information to create the attachment.</param>
+        /// <summary>
+        /// Creates a new attachment.
+        /// </summary>
+        /// <param name="createAttachmentDto">The data transfer object for creating an attachment.</param>
         /// <returns>The ID of the created attachment.</returns>
         Task<string> CreateAttachment(CreateAttachmentDto createAttachmentDto);
-        
-        /// Retrieves an attachment by its request details.
-        /// <param name="attachmentRequestDto">The data transfer object containing the request details to retrieve the attachment.</param>
+
+        /// <summary>
+        /// Retrieves an attachment by its request data.
+        /// </summary>
+        /// <param name="attachmentRequestDto">The data transfer object for requesting an attachment.</param>
         /// <returns>The requested attachment.</returns>
         Task<Attachment> GetAttachment(AttachmentRequestDto attachmentRequestDto);
-        
+
+        /// <summary>
         /// Updates an existing attachment.
-        /// <param name="updateAttachmentDto">The data transfer object containing information to update the attachment.</param>
+        /// </summary>
+        /// <param name="updateAttachmentDto">The data transfer object for updating an attachment.</param>
         /// <returns>The ID of the updated attachment.</returns>
         Task<string> UpdateAttachment(UpdateAttachmentDto updateAttachmentDto);
-        
+
+        /// <summary>
         /// Deletes an attachment.
-        /// <param name="deleteAttachmentDto">The data transfer object containing information to delete the attachment.</param>
-        /// <returns>True if the attachment was deleted successfully, otherwise false.</returns>
+        /// </summary>
+        /// <param name="deleteAttachmentDto">The data transfer object for deleting an attachment.</param>
+        /// <returns>True if the attachment was deleted, false otherwise.</returns>
         Task<bool> DeleteAttachment(DeleteAttachmentDto deleteAttachmentDto);
-        
-        /// Retrieves a list of attachments based on the request details.
-        /// <param name="listAttachmentRequestDto">The data transfer object containing the request details to retrieve the list of attachments.</param>
+
+        /// <summary>
+        /// Retrieves a list of attachments based on the request data.
+        /// </summary>
+        /// <param name="listAttachmentRequestDto">The data transfer object for requesting a list of attachments.</param>
         /// <returns>A list of attachments.</returns>
         Task<List<Attachment>> GetListAttachment(ListAttachmentRequestDto listAttachmentRequestDto);
+
+        /// <summary>
+        /// Handles an attachment, optionally associating it with a GUID and performing an action.
+        /// </summary>
+        /// <param name="createAttachmentDto">The data transfer object for creating an attachment.</param>
+        /// <param name="guid">An optional GUID to associate with the attachment.</param>
+        /// <param name="action">An action to perform with the GUID.</param>
+        Task HandleAttachement(CreateAttachmentDto createAttachmentDto, Guid? guid, Action<Guid?> action);
     }
 }
