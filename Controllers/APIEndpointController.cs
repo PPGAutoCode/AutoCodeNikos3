dffa140dcd7be7bf49e8ec759b36182357f5944b
AutoCode <autocode@papergo.io>
2024-07-19 15:22:45 +0300

    update from autocode on 2024-07-19 15:22:45.491620

--- Controllers/APIEndpointController.cs
+++ Controllers/APIEndpointController.cs
@@ -18,37 +18,45 @@ namespace ProjectName.Controllers
             _apiEndpointService = apiEndpointService;
         }
 
-        // [HttpPost("create")]
-        // public async Task<IActionResult> CreateAPIEndpoint([FromBody] Request<CreateAPIEndpointDto> request)
-        // {
-        //     return await SafeExecutor.ExecuteAsync(async () =>
-        //     {
-        //         var result = await _apiEndpointService.CreateAPIEndpoint(request.Payload);
-        //         return Ok(new Response<string> { Payload = result });
-        //     });
-        // }
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAPIEndpoint([FromBody] Request<CreateAPIEndpointDto> request)
+        {
+            return await SafeExecutor.ExecuteAsync(async () =>
+            {
+                var result = await _apiEndpointService.CreateAPIEndpoint(request.Payload);
+                return Ok(new Response<string> { Payload = result });
+            });
+        }
 
-        // [HttpPost("get")]
-        // public async Task<IActionResult> GetAPIEndpoint([FromBody] Request<APIEndpointRequestDto> request)
-        // {
-        //     return await SafeExecutor.ExecuteAsync(async () =>
-        //     {
-        //         var result = await _apiEndpointService.GetAPIEndpoint(request.Payload);
-        //         return Ok(new Response<APIEndpoint> { Payload = result });
-        //     });
-        // }
+        [HttpPost("get")]
+        public async Task<IActionResult> GetAPIEndpoint([FromBody] Request<APIEndpointRequestDto> request)
+        {
+            return await SafeExecutor.ExecuteAsync(async () =>
+            {
+                var result = await _apiEndpointService.GetAPIEndpoint(request.Payload);
+                return Ok(new Response<APIEndpoint> { Payload = result });
+            });
+        }
 
-       
+        [HttpPost("update")]
+        public async Task<IActionResult> UpdateAPIEndpoint([FromBody] Request<UpdateAPIEndpointDto> request)
+        {
+            return await SafeExecutor.ExecuteAsync(async () =>
+            {
+                var result = await _apiEndpointService.UpdateAPIEndpoint(request.Payload);
+                return Ok(new Response<string> { Payload = result });
+            });
+        }
 
-        // [HttpPost("delete")]
-        // public async Task<IActionResult> DeleteAPIEndpoint([FromBody] Request<DeleteAPIEndpointDto> request)
-        // {
-        //     return await SafeExecutor.ExecuteAsync(async () =>
-        //     {
-        //         var result = await _apiEndpointService.DeleteAPIEndpoint(request.Payload);
-        //         return Ok(new Response<bool> { Payload = result });
-        //     });
-        // }
+        [HttpPost("delete")]
+        public async Task<IActionResult> DeleteAPIEndpoint([FromBody] Request<DeleteAPIEndpointDto> request)
+        {
+            return await SafeExecutor.ExecuteAsync(async () =>
+            {
+                var result = await _apiEndpointService.DeleteAPIEndpoint(request.Payload);
+                return Ok(new Response<bool> { Payload = result });
+            });
+        }
 
         [HttpPost("list")]
         public async Task<IActionResult> GetListAPIEndpoint([FromBody] Request<ListAPIEndpointRequestDto> request)
--- Interfaces/IAPIEndpointService.cs
+++ Interfaces/IAPIEndpointService.cs
@@ -15,28 +15,28 @@ namespace ProjectName.Interfaces
         /// </summary>
         /// <param name="createAPIEndpointDto">Data transfer object for creating an API endpoint.</param>
         /// <returns>The identifier of the created API endpoint.</returns>
-        //Task<string> CreateAPIEndpoint(CreateAPIEndpointDto createAPIEndpointDto);
+        Task<string> CreateAPIEndpoint(CreateAPIEndpointDto createAPIEndpointDto);
 
         /// <summary>
         /// Retrieves an API endpoint by its request details.
         /// </summary>
         /// <param name="apiEndpointRequestDto">Data transfer object for requesting an API endpoint.</param>
         /// <returns>The requested API endpoint.</returns>
-       //Task<APIEndpoint> GetAPIEndpoint(APIEndpointRequestDto apiEndpointRequestDto);
+       Task<APIEndpoint> GetAPIEndpoint(APIEndpointRequestDto apiEndpointRequestDto);
 
         /// <summary>
         /// Updates an existing API endpoint.
         /// </summary>
         /// <param name="updateAPIEndpointDto">Data transfer object for updating an API endpoint.</param>
         /// <returns>The identifier of the updated API endpoint.</returns>
-        //Task<string> UpdateAPIEndpoint(UpdateAPIEndpointDto updateAPIEndpointDto);
+        Task<string> UpdateAPIEndpoint(UpdateAPIEndpointDto updateAPIEndpointDto);
 
         /// <summary>
         /// Deletes an API endpoint.
         /// </summary>
         /// <param name="deleteAPIEndpointDto">Data transfer object for deleting an API endpoint.</param>
         /// <returns>True if the API endpoint was deleted successfully, otherwise false.</returns>
-        //Task<bool> DeleteAPIEndpoint(DeleteAPIEndpointDto deleteAPIEndpointDto);
+        Task<bool> DeleteAPIEndpoint(DeleteAPIEndpointDto deleteAPIEndpointDto);
 
         /// <summary>
         /// Retrieves a list of API endpoints based on the request details.
