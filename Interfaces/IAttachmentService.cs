42e8c6e4b9bbe44655136ba35f5ea38df5df831b
AutoCode <autocode@papergo.io>
2024-07-23 17:27:03 +0300

    update from autocode on 2024-07-23 17:27:03.909453

--- Interfaces/IAttachmentService.cs
+++ Interfaces/IAttachmentService.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -13,44 +14,50 @@ namespace ProjectName.Interfaces
         /// <summary>
         /// Creates a new attachment.
         /// </summary>
-        /// <param name="createAttachmentDto">Data transfer object for creating an attachment.</param>
+        /// <param name="createAttachmentDto">The data transfer object for creating an attachment.</param>
         /// <returns>The ID of the created attachment.</returns>
         Task<string> CreateAttachment(CreateAttachmentDto createAttachmentDto);
 
         /// <summary>
-        /// Retrieves an attachment by its request details.
+        /// Retrieves an attachment by its request data.
         /// </summary>
-        /// <param name="attachmentRequestDto">Data transfer object for requesting an attachment.</param>
+        /// <param name="attachmentRequestDto">The data transfer object for requesting an attachment.</param>
         /// <returns>The requested attachment.</returns>
         Task<Attachment> GetAttachment(AttachmentRequestDto attachmentRequestDto);
 
         /// <summary>
         /// Updates an existing attachment.
         /// </summary>
-        /// <param name="updateAttachmentDto">Data transfer object for updating an attachment.</param>
+        /// <param name="updateAttachmentDto">The data transfer object for updating an attachment.</param>
         /// <returns>The ID of the updated attachment.</returns>
         Task<string> UpdateAttachment(UpdateAttachmentDto updateAttachmentDto);
 
         /// <summary>
         /// Deletes an attachment.
         /// </summary>
-        /// <param name="deleteAttachmentDto">Data transfer object for deleting an attachment.</param>
-        /// <returns>True if the attachment was deleted successfully, otherwise false.</returns>
+        /// <param name="deleteAttachmentDto">The data transfer object for deleting an attachment.</param>
+        /// <returns>True if the attachment was deleted, false otherwise.</returns>
         Task<bool> DeleteAttachment(DeleteAttachmentDto deleteAttachmentDto);
 
         /// <summary>
-        /// Retrieves a list of attachments based on the request details.
+        /// Retrieves a list of attachments based on the request data.
         /// </summary>
-        /// <param name="listAttachmentRequestDto">Data transfer object for requesting a list of attachments.</param>
+        /// <param name="listAttachmentRequestDto">The data transfer object for requesting a list of attachments.</param>
         /// <returns>A list of attachments.</returns>
         Task<List<Attachment>> GetListAttachment(ListAttachmentRequestDto listAttachmentRequestDto);
 
         /// <summary>
-        /// Handles an attachment, optionally associating it with a GUID and performing an action.
+        /// Handles an attachment, which can involve creating a new one or updating an existing one.
         /// </summary>
-        /// <param name="createAttachmentDto">Data transfer object for creating an attachment.</param>
-        /// <param name="guid">Optional GUID to associate with the attachment.</param>
-        /// <param name="action">Action to perform with the GUID.</param>
-        Task HandleAttachment(CreateAttachmentDto createAttachmentDto, Guid? guid, Action<Guid?> action);
+        /// <param name="newAttachment">The data transfer object for creating a new attachment.</param>
+        /// <param name="existingAttachmentId">The ID of an existing attachment to update, if applicable.</param>
+        /// <param name="updateAttachmentFieldId">An action to update the attachment field ID.</param>
+        Task HandleAttachment(CreateAttachmentDto newAttachment, Guid? existingAttachmentId, Action<Guid?> updateAttachmentFieldId);
+
+        /// <summary>
+        /// Uploads an attachment.
+        /// </summary>
+        /// <param name="newAttachment">The data transfer object for creating a new attachment.</param>
+        Task UploadAttachment(CreateAttachmentDto newAttachment);
     }
 }
