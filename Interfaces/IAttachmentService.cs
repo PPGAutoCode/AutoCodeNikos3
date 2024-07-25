2cdcc1046c6ed6be45126f1628a1c1dc43b73a25
AutoCode <autocode@papergo.io>
2024-07-25 17:19:42 +0300

    update from autocode on 2024-07-25 17:19:42.357830

--- Interfaces/IAttachmentService.cs
+++ Interfaces/IAttachmentService.cs
@@ -6,51 +6,50 @@ using ProjectName.Types;
 namespace ProjectName.Interfaces
 {
     /// <summary>
-    /// Interface for managing attachments, providing CRUD and list operations.
+    /// Interface for managing attachment-related operations.
     /// </summary>
     public interface IAttachmentService
     {
         /// <summary>
-        /// Creates a new attachment based on the provided data transfer object.
+        /// Creates a new attachment.
         /// </summary>
-        /// <param name="createAttachmentDto">The data transfer object containing the information to create the attachment.</param>
+        /// <param name="createAttachmentDto">Data transfer object for creating an attachment.</param>
         /// <returns>A string representing the result of the creation operation.</returns>
         Task<string> CreateAttachment(CreateAttachmentDto createAttachmentDto);
 
         /// <summary>
-        /// Retrieves an attachment based on the provided request data transfer object.
+        /// Retrieves an attachment based on the provided request data.
         /// </summary>
-        /// <param name="attachmentRequestDto">The data transfer object containing the request information to retrieve the attachment.</param>
-        /// <returns>An Attachment object representing the retrieved attachment.</returns>
+        /// <param name="attachmentRequestDto">Data transfer object for requesting an attachment.</param>
+        /// <returns>An Attachment object.</returns>
         Task<Attachment> GetAttachment(AttachmentRequestDto attachmentRequestDto);
 
         /// <summary>
-        /// Updates an existing attachment based on the provided data transfer object.
+        /// Updates an existing attachment.
         /// </summary>
-        /// <param name="updateAttachmentDto">The data transfer object containing the information to update the attachment.</param>
+        /// <param name="updateAttachmentDto">Data transfer object for updating an attachment.</param>
         /// <returns>A string representing the result of the update operation.</returns>
         Task<string> UpdateAttachment(UpdateAttachmentDto updateAttachmentDto);
 
         /// <summary>
-        /// Deletes an attachment based on the provided data transfer object.
+        /// Deletes an attachment.
         /// </summary>
-        /// <param name="deleteAttachmentDto">The data transfer object containing the information to delete the attachment.</param>
+        /// <param name="deleteAttachmentDto">Data transfer object for deleting an attachment.</param>
         /// <returns>A boolean indicating the success of the deletion operation.</returns>
         Task<bool> DeleteAttachment(DeleteAttachmentDto deleteAttachmentDto);
 
         /// <summary>
-        /// Retrieves a list of attachments based on the provided request data transfer object.
+        /// Retrieves a list of attachments based on the provided request data.
         /// </summary>
-        /// <param name="listAttachmentRequestDto">The data transfer object containing the request information to retrieve the list of attachments.</param>
-        /// <returns>A list of Attachment objects representing the retrieved attachments.</returns>
+        /// <param name="listAttachmentRequestDto">Data transfer object for requesting a list of attachments.</param>
+        /// <returns>A list of Attachment objects.</returns>
         Task<List<Attachment>> GetListAttachment(ListAttachmentRequestDto listAttachmentRequestDto);
 
         /// <summary>
-        /// Upserts an attachment based on the provided data transfer object. This operation will either update an existing attachment or create a new one if it does not exist.
+        /// Upserts an attachment. This operation will update the attachment if it exists or create a new one if it does not.
         /// </summary>
-        /// <param name="updateAttachmentDto">The data transfer object containing the information to upsert the attachment.</param>
-        /// <param name="attachment">The optional Attachment object to be used in the upsert operation.</param>
+        /// <param name="updateAttachmentDto">Data transfer object for updating an attachment.</param>
         /// <returns>A string representing the result of the upsert operation.</returns>
-        Task<string> UpsertAttachment(UpdateAttachmentDto updateAttachmentDto, Attachment attachment = null);
+        Task<string> UpsertAttachment(UpdateAttachmentDto updateAttachmentDto);
     }
 }
