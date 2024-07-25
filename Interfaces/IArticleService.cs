6eead423de2bd1e9f3aef9b91918006a2ed3d9ee
AutoCode <autocode@papergo.io>
2024-07-25 16:59:00 +0300

    update from autocode on 2024-07-25 16:59:00.725789

--- Interfaces/IArticleService.cs
+++ Interfaces/IArticleService.cs
@@ -10,39 +10,39 @@ namespace ProjectName.Interfaces
     /// </summary>
     public interface IArticleService
     {
-        // /// <summary>
-        // /// Creates a new article based on the provided data.
-        // /// </summary>
-        // /// <param name="createArticleDto">Data transfer object containing the information needed to create an article.</param>
-        // /// <returns>A string representing the result of the creation operation.</returns>
-        // Task<string> CreateArticle(CreateArticleDto createArticleDto);
-        
         /// <summary>
-        /// Retrieves an article based on the provided request data.
+        /// Asynchronously creates a new article.
         /// </summary>
-        /// <param name="articleRequestDto">Data transfer object containing the information needed to retrieve an article.</param>
-        /// <returns>An Article object representing the retrieved article.</returns>
+        /// <param name="createArticleDto">The data transfer object containing the information for the new article.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the ID of the created article as a string.</returns>
+        Task<string> CreateArticle(CreateArticleDto createArticleDto);
+
+        /// <summary>
+        /// Asynchronously retrieves an article based on the provided request data.
+        /// </summary>
+        /// <param name="articleRequestDto">The data transfer object containing the request information for the article.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the requested article.</returns>
         Task<Article> GetArticle(ArticleRequestDto articleRequestDto);
-        //
-        // /// <summary>
-        // /// Updates an existing article based on the provided data.
-        // /// </summary>
-        // /// <param name="updateArticleDto">Data transfer object containing the information needed to update an article.</param>
-        // /// <returns>A string representing the result of the update operation.</returns>
-        // Task<string> UpdateArticle(UpdateArticleDto updateArticleDto);
-        //
-        // /// <summary>
-        // /// Deletes an article based on the provided data.
-        // /// </summary>
-        // /// <param name="deleteArticleDto">Data transfer object containing the information needed to delete an article.</param>
-        // /// <returns>A boolean indicating whether the deletion was successful.</returns>
-        // Task<bool> DeleteArticle(DeleteArticleDto deleteArticleDto);
-        //
-        // /// <summary>
-        // /// Retrieves a list of articles based on the provided request data.
-        // /// </summary>
-        // /// <param name="listArticleRequestDto">Data transfer object containing the information needed to retrieve a list of articles.</param>
-        // /// <returns>A list of Article objects representing the retrieved articles.</returns>
-        // Task<List<Article>> GetListArticle(ListArticleRequestDto listArticleRequestDto);
+
+        /// <summary>
+        /// Asynchronously updates an existing article.
+        /// </summary>
+        /// <param name="updateArticleDto">The data transfer object containing the updated information for the article.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the ID of the updated article as a string.</returns>
+        Task<string> UpdateArticle(UpdateArticleDto updateArticleDto);
+
+        /// <summary>
+        /// Asynchronously deletes an article based on the provided deletion data.
+        /// </summary>
+        /// <param name="deleteArticleDto">The data transfer object containing the information for the article to be deleted.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a boolean indicating the success of the deletion operation.</returns>
+        Task<bool> DeleteArticle(DeleteArticleDto deleteArticleDto);
+
+        /// <summary>
+        /// Asynchronously retrieves a list of articles based on the provided request data.
+        /// </summary>
+        /// <param name="listArticleRequestDto">The data transfer object containing the request information for the list of articles.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of the requested articles.</returns>
+        Task<List<Article>> GetListArticle(ListArticleRequestDto listArticleRequestDto);
     }
 }
